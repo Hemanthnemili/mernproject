a2306e3b0f2f108e0b833af2e2c1fd505e810417
Hemanthnemili <hemanth.nemili@gmail.com>
2023-10-14 11:26:54 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+
 export default function App() {
   console.log("hemanth");
   return (
     <div>
-      <h1 className="text-3xl font-bold underline hover:text-red-400">
-        Hemanth
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
