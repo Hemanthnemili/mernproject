43ba3828358b7095d3c52649e8b1059394da0866
Hemanthnemili <hemanth.nemili@gmail.com>
2023-10-14 11:12:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  console.log("hemanth");
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline hover:text-red-400">
+        Hemanth
+      </h1>
+    </div>
+  );
+}
