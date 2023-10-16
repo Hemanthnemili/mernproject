f8e2054c34790df7b42b0658221bdbdd16889018
Hemanthnemili <hemanth.nemili@gmail.com>
2023-10-16 17:31:32 +0530

    complete signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
-  return <div>Signup</div>;
+  const [fromData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...fromData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fromData),
+    });
+    const data = await res.json();
+    navigate("/signin");
+    console.log(data);
+    if (data.success === true) {
+      setLoading(true);
+      return;
+    }
+  };
+  return (
+    <div className=" p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-3xl font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          id="email"
+          onChange={handleChange}
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          onChange={handleChange}
+          id="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 rounded-lg p-3 text-white uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-3 mt-5">
+        <p>Have an Account?</p>
+        <Link to="/signin">
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,25 +5,25 @@ import generateTokenAndSetCookie from "../helpers/generateTokenAndSetCookie.js";
 const signIn = async (req, res) => {
   try {
     const { username, password, email } = req.body;
-    const user = await User.findOne({ username, email });
-    if (user) return res.status(401).json({ error: "User Already Exists" });
+    // const user = await User.findOne({ username, email });
+    // if (user) return res.status(401).json({ error: "User Already Exists" });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({ username, password: hashedPassword, email });
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    // res.status(201).json({ message: "User created successfully" });
 
-    // if (newUser) {
-    //   generateTokenAndSetCookie(newUser._id, res);
-    //   res.status(201).json({
-    //     _id: newUser._id,
-    //     username: newUser.username,
-    //     email: newUser.email,
-    //   });
-    // } else {
-    //   res.status(404).json({ error: "Invalid user Data" });
-    // }
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      });
+    } else {
+      res.status(404).json({ error: "Invalid user Data" });
+    }
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
