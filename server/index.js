9e6652ece5c85043748a407ca502e55e12e55593
Hemanthnemili <hemanth.nemili@gmail.com>
2023-10-16 11:29:51 +0530

    added database

--- server/index.js
+++ server/index.js
@@ -1,15 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
+import connectDb from "../server/db/connectDb.js";
+connectDb();
 
 const app = express();
-
-const mongo =
-  "mongodb+srv://test1:test111@project.zzshlyu.mongodb.net/project?retryWrites=true&w=majority";
-
-mongoose
-  .connect(mongo)
-  .then(() => console.log("connected"))
-  .catch((e) => console.log(e.message));
 
 app.listen(5000, () => {
   console.log("listening on port");
